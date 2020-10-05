8e74bc9ed0f26a0243f64b10f4460ae6ca6cf68b
iftitutul <iftikhar.tutul@gmail.com>
2020-10-05 13:42:54 +0600

    final test

--- Week-2/2.4-final-test-generate_report.py
+++ Week-2/2.4-final-test-generate_report.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import csv
 
+# We also need to pass a dialect as a parameter to this function. There isn't a well-defined standard for comma-separated value files, so the parser needs to be flexible. Flexibility here means that there are many parameters to control how csv parses or writes data. Rather than passing each of these parameters to the reader and writer separately, we group them together conveniently into a dialect object. 
+# Dialect classes can be registered by name so that callers of the CSV module don't need to know the parameter settings in advance. We will now register a dialect empDialect.
 def read_employees(csv_file_location):
    csv.register_dialect('empDialect', skipinitialspace=True, strict=True)
    employee_file = csv.DictReader(open(csv_file_location), dialect='empDialect')
